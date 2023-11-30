99c94490030c25d16e49311e52980b61677abbcc
nurwafiqah <nurwafiqah2506003@gmail.com>
2023-11-30 12:54:23 +0700

    first commit

---
+++ app/views/dashboard/index.php
@@ -0,0 +1,10 @@
+<h2>Dashboard</h2>
+
+<div class="info">Selamat datang, <strong>Nur Wafiqah Azizah Sitompul</strong></div>
+
+<p align="center"><strong>Di zaman sekarang yang serba modern, semua kegiatan manusia ditunjang oleh energi listrik. 
+Energi ini menjadi sangat penting karena hampir semua peralatan yang kita gunakan bersumber dari listrik. 
+Mulai dari lampu, lemari es, penanak nasi, AC, bahkan smartphone membutuhkan tenaga listrik agar bisa menyala.</strong></p>
+
+<p align="center"><strong>Sebagai pengguna sekaligus warga negara yang bijak, kita harus selalu membayar listrik tepat waktu. 
+Peraturan PLN menetapkan pascabayar harus dilakukan paling lambat tanggal 20 setiap bulannya.</strong></p>
